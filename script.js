645e905e0102b1e11e12164f3dcb986cf6bca73c
Romain Wirth <romain.wirth@gmail.com>
2025-01-06 23:09:00 +0100

    Création fonction combattre et ajout de magie

--- script.js
+++ script.js
@@ -1,9 +1,87 @@
-let pseudoInput = document.querySelector("#pseudoInput");
-let choisirPseudo = document.querySelector("#choisirPseudo");
 let personnagePrincipal;
 let pseudo;
 let classe;
 let monstreChoisi;
+let actionChoisie;
+
+let deroulementCombat = document.querySelector("#deroulementCombat");
+
+class Personnage {
+    constructor(pseudo, classe, sante, attaque) {
+    this.pseudo = pseudo;
+    this.classe = classe;
+    this.sante = sante;
+    this.attaque = attaque;
+    this.magie = 0;
+}
+
+
+    attaquer(personnage) {
+        personnage.sante -= this.attaque;
+        this.magie ++;
+        personnage.verifierSante();
+        deroulementCombat.innerHTML = `${this.pseudo} attaque ${personnage.pseudo} et lui inflige ${this.attaque} dégats.`;
+        deroulementCombat.style.color = "green";
+    }
+
+    attaqueSpeciale(personnage) {
+        let attaqueSpecialeError = document.querySelector("#attaqueSpecialeError");
+        if (this.magie >= 5) {
+            personnage.sante -= this.attaque * 5;
+            this.magie = 0
+            attaqueSpecialeError.innerHTML = "";
+            personnage.verifierSante();
+            deroulementCombat.innerHTML = `${this.pseudo} attaque ${personnage.pseudo} et lui inflige ${this.attaque * 5} dégats.`;
+            deroulementCombat.style.color = "green";
+        }
+        else
+        attaqueSpecialeError.innerHTML = "Vous avez besoin de 5 points de magie pour lancer une attaque spéciale";
+        attaqueSpecialeError.style.color = "red";
+        return
+    }        
+
+    attendre() {
+        this.magie = this.magie + 3;
+        deroulementCombat.innerHTML = `${this.pseudo} attendre et se prépare pour le prochain tour. Il gagne 3 points de magie.`;
+        deroulementCombat.style.color = "green";
+    }
+
+// Ajouter fonction défendre
+
+    seSoigner() {
+        // ajouter paramètre santeMax au personnage pour soigner un % des points de vie maximum
+        this.sante += 50;
+        deroulementCombat.innerHTML = `${this.pseudo} se soigner et récupère 50 points de vie`
+        deroulementCombat.style.color = "green";
+    }
+    
+    
+    verifierSante() {
+        if (this.sante <= 0) {
+            this.sante = 0;
+            alert(`${this.pseudo} est vaincu!`);
+        } 
+    }
+}
+class Guerrier extends Personnage {
+    constructor(pseudo, classe) {
+        super(pseudo, classe, 350, 40);
+}
+}
+
+class Mage extends Personnage {
+    constructor(pseudo, classe) {
+        super(pseudo, classe, 200, 70);
+    }
+
+}
+
+class Monstre extends Personnage {
+    constructor (pseudo, sante, attaque ) {
+    super(pseudo, "Monstre", sante, attaque)
+       
+}
+}
 
 function creerPersonnage(classe) {
     switch (classe) {
@@ -14,9 +92,6 @@ function creerPersonnage(classe) {
         return new Mage(pseudo, classe);
     }
 }
-
-
-
 
 function choisirMonstre() {
     let monstres = [
@@ -39,54 +114,58 @@ function choisirMonstre() {
             choixMonstre.style.display = "none";
 
             monstreChoisi = monstre
+            combattre()
         }
         )
      } 
     )
          }
 
-class Personnage {
-    constructor(pseudo, classe, sante, attaque) {
-    this.pseudo = pseudo;
-    this.classe = classe;
-    this.sante = sante;
-    this.attaque = attaque
+function combattre() {
+    let containerChoixAction = document.querySelector("#containerChoixAction");
+
+    containerChoixAction.textContent = "Que voulez-vous faire?";
+
+    let actions = ["Attaquer", "Lancer une attaque spéciale", "Attendre", "Se soigner"];
+    
+    actions.forEach ((action) => {
+        let choixAction = document.createElement("button");
+        choixAction.textContent = action;
+        containerChoixAction.append(choixAction);
+  
+    choixAction.addEventListener("click", () => {
+    switch(action) {
+        case "Attaquer" :
+            personnagePrincipal.attaquer(monstreChoisi);
+            break
+        case "Lancer une attaque spéciale" :
+            personnagePrincipal.attaqueSpeciale(monstreChoisi);
+            break
+        case "Attendre" : 
+            personnagePrincipal.attendre();
+            break
+        case "Se soigner":
+            personnagePrincipal.seSoigner();
+            break
+            }
+  })
+        })
+    
 }
 
-get informations() {
-    return `${this.pseudo} ${this.classe} ${this.sante} ${this.attaque}`
-}
-
-    attaquer() {}
-    defendre() {}
-    seSoigner() {}
-    }
-
-
-class Guerrier extends Personnage {
-    constructor(pseudo, classe) {
-        super(pseudo, classe, 350, 40);
-}
-}
-
-class Mage extends Personnage {
-    constructor(pseudo, classe) {
-        super(pseudo, classe, 200, 70);
-    }
-
-}
-
-class Monstre extends Personnage {
-    constructor (pseudo, sante, attaque ) {
-    super(pseudo, "Monstre", sante, attaque)
-       
-}
-}
-
+let pseudoInput = document.querySelector("#pseudoInput");
+let choisirPseudo = document.querySelector("#choisirPseudo");
 
 choisirPseudo.addEventListener("submit", (e) => {
     e.preventDefault()
         pseudo = pseudoInput.value.trim();
+        if (!pseudo) {
+         let pseudoError = document.querySelector("#pseudoError");
+         pseudoError.style.display = "block";
+         return
+        }
+
+        else
         choisirPseudo.style.display = "none";
         let containerChoixGuerrier = document.querySelector("#containerChoixGuerrier");
         let choixGuerrier = document.createElement("button");
@@ -104,8 +183,8 @@ choisirPseudo.addEventListener("submit", (e) => {
        choixGuerrier.addEventListener("click", () => {
             classe = "Guerrier"
             personnagePrincipal = creerPersonnage(classe);
-            choixGuerrier.remove()
-            choixMage.remove;
+            choixGuerrier.style.display = "none";
+            choixMage.style.display = "none";
             choisirMonstre()
             }
         
@@ -114,7 +193,7 @@ choisirPseudo.addEventListener("submit", (e) => {
             classe = "Mage"
             personnagePrincipal = creerPersonnage(classe);
             choixGuerrier.style.display = "none";
-            choixMage.style.display = "none"
+            choixMage.style.display = "none";
             choisirMonstre()
             }
         )
